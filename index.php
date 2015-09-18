de5543d046ab3df0a6ab88b150e770eeb0c38375
Daniel Racine <daniel.racine@danwebco.ca>
2015-09-17 22:06:30 -0400

    shorten url twitter sharing

--- index.php
+++ index.php
@@ -81,8 +81,6 @@
     <meta name="twitter:title" <?php echo "content=\"" . _( 'head-og-title' ) . "\""; ?>/>
     <meta name="twitter:description" <?php echo "content=\"" . _( 'head-og-description' ) . "\""; ?>/>
     <meta name="twitter:image" <?php echo "content=\"" . _( 'head-og-image' ) . "\""; ?>/>
-    <!-- <meta name="twitter:image:width" content="560">
-    <meta name="twitter:image:height" content="560"> -->
 
 
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory
